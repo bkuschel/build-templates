5686eb76527905ad4f1324f414436d6d1a141763
Boris Kuschel <kuschel@ca.ibm.com>
2018-11-05 16:03:08 -0500

    Helper Script for IBM Cloud Container Registry Service

---
+++ icr/README.md
@@ -0,0 +1,118 @@
+# IBM Container Registry Helper 
+
+This script provisions (or verifies) an IBM Cloud service ID with
+permissions as secified by the policy.json file. The user must be logged into 
+IBM Cloud with a user that has the `Administrator` role assigned to the 
+`container-registry` service for the region(s). See `iamcloud iam user-policy-create` 
+
+By default, the following resources will be provisioned:
+
+* An IBM Cloud Service ID named `knative-builder` with appropriate permissions.
+* A Kubernetes service account (named `builder` by default) with secrets to
+  enable pushing to IBM Cloud using the `knative-builder`'s credentials.
+
+The default policy.json file allow images to be built and pushed into the registry of 
+any region and registry namepsace. (see @policy.json)
+It is possible to restrict the policy to a region and a registry namespace as follows, multiple
+specific regions' attributes can be added to the `resources` array.
+
+```json
+{
+    "roles": [
+        {
+            "role_id": "crn:v1:bluemix:public:iam::::serviceRole:Writer"
+        }
+    ],
+    "resources": [
+        {
+            "attributes": [
+                {
+                    "name": "serviceName",
+                    "value": "container-registry"
+                },
+                {
+                    "name": "region",
+                    "value": "us-east"
+                },
+                {
+                    "name": "resourceType",
+                    "value": "namespace"
+                },
+                {
+                    "name": "resource",
+                    "value": "mynamespace"
+                }
+            ]
+        }
+    ]
+}
+```
+
+To use, simply add a `serviceAccountName: builder` entry to your build definition
+
+```yaml:
+apiVersion: build.knative.dev/v1alpha1
+kind: Build
+metadata:
+  name: mybuild
+spec:
+  serviceAccountName: builder
+  source: ...
+  template: ...
+```
+
+## Usage
+
+Should be run from the repository root directory.
+
+```shell
+# Usage assumes that the user has IAM Owner permissions for the project.
+icr/helper.sh
+```
+
+Optionally, `helper.sh` accepts two positional arguments to specify
+the namespace and kubernetes service account used:
+
+```shell
+icr/helper.sh $MY_KUBE_NAMESPACE builder-serviceaccount
+```
+
+This will output a log of operations performed or skipped:
+
+```
+Could not find knative-builder, creating...
+Creating service ID knative-builder bound to current account as kuschel@ca.ibm.com...
+OK
+Service ID knative-builder is created successfully
+
+Name          knative-builder
+Description   Knative Builder
+CRN           crn:v1:bluemix:public:iam-identity::a/790c0808c946b9e15cc2e63013fded9d::serviceid:ServiceId-14483a83-4079-4103-9c1e-1879834feddb
+Bound To      crn:v1:bluemix:public:::a/790c0808c946b9e15cc2e63013fded9d:::
+Version       1-6684c1b5255d0c300b25b4268a97fd9c
+Locked        false
+UUID          ServiceId-14483a83-4079-4103-9c1e-1879834feddb
+Creating policy under current account for service ID knative-builder as kuschel@ca.ibm.com...
+OK
+Service policy is successfully created
+
+
+Policy ID:   e167c8ff-5e29-4d7a-a671-f50868a4e2e9
+Version:     1-9004ae95ef74d0b1c6e5adf9bdd35a4b
+Roles:       Writer
+Resources:
+             Service Name       container-registry
+             Service Instance
+             Region
+             Resource Type
+             Resource
+
+
+
+Creating API key knative-builder of service knative-builder as kuschel@ca.ibm.com...
+OK
+Service API key knative-builder is created
+Successfully save API key information to knative-builder-key.json
+serviceaccount/builder created
+secret/icr-creds created
+```
---
+++ icr/helper.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+#
+# Copyright 2018 The Knative Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# A simple script to create or validate credentials to allow pushing
+# built images to ECR.
+#
+# This script assumes the following environment:
+#
+# 1. `ibmcloud`, 'kubectl` installed and in $PATH.
+#
+# 2. User authenticated ibmcloud session (ie. `ibmcloud login`)
+#
+# 3. The authenticated user has to have the Administrator platform role 
+#    for the container-registry service for the regions in which the service ID
+#    will have access to the registry. Read README.MD for details on service ID's policy.
+#
+# The script should warn if any of these preconditions cannot be met.
+#
+# Once all arguments are validated, this script will create a kubernetes
+# secret with the appropriate metadata for usage by build steps, accessible
+# by a service account named "builder" (by default).
+#
+##
+## Validate environment.
+##
+
+checkBinary() {
+    if ! which $1 >&/dev/null; then
+        echo "Unable to locate $1, please ensure it is installed and on your \$PATH."
+        exit 1
+    fi
+}
+
+checkBinary ibmcloud
+checkBinary kubectl
+readonly KUBECTL_FLAGS="${1:+ -n $1}"
+
+if ! kubectl $KUBECTL_FLAGS get sa >& /dev/null; then
+    echo "Unable to read Kubernetes service accounts with 'kubectl $KUBECTL_FLAGS get sa'."
+    exit 1
+fi
+
+readonly KUBE_SA=${2:-"builder"}
+
+
+##
+## Begin ...
+##
+
+: ${IBMCLOUD_SERVICEID_NAME:=knative-builder}
+
+# Supress stderr, as many of the check queries will print extra output
+# if the resources are not present. Keep stderr on FD 3 to allow
+# printing output from explicit create commands.
+exec 3>&2
+exec 2>/dev/null
+
+# See if secrets are already loaded. If not, add them.
+if [[ $(kubectl $KUBECTL_FLAGS get -o jsonpath='{.secrets[?(@.name=="icr-creds")].name}' sa $KUBE_SA) == 'icr-creds' ]]; then
+    echo "Found serviceAccount '$KUBE_SA' with access to 'icr-creds'"
+    if [[ $(kubectl $KUBECTL_FLAGS get -o jsonpath={.type} secrets icr-creds) == 'kubernetes.io/basic-auth' ]]; then
+	echo "Secrets set up already, exiting"
+	exit 0
+    fi
+fi
+
+if ibmcloud iam service-id $IBMCLOUD_SERVICEID_NAME >&/dev/null; then
+    echo "Using existing service account $IBMCLOUD_SERVICEID_NAME"
+else
+    echo "Could not find $IBMCLOUD_SERVICEID_NAME, creating..."
+    ibmcloud iam service-id-create $IBMCLOUD_SERVICEID_NAME -d "Knative Builder"
+fi
+
+ibmcloud iam service-policy-create $IBMCLOUD_SERVICEID_NAME --file icr/policy.json 2>&3 || exit 2
+
+# Create the API Key key for the service account.
+ibmcloud iam service-api-key-create $IBMCLOUD_SERVICEID_NAME $IBMCLOUD_SERVICEID_NAME --file knative-builder-key.json 2>&3 || exit 2
+
+cat <<EOF | kubectl $KUBECTL_FLAGS apply -f - 2>&3
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: $KUBE_SA
+secrets:
+- name: icr-creds
+---
+apiVersion: v1
+kind: Secret
+metadata:
+  name: icr-creds
+  annotations:
+    build.knative.dev/docker-0: https://registry.ng.bluemix.net
+    build.knative.dev/docker-1: https://registry.eu-de.bluemix.net
+    build.knative.dev/docker-2: https://registry.eu-gb.bluemix.net
+    build.knative.dev/docker-3: https://registry.au-syd.bluemix.net
+type: kubernetes.io/basic-auth
+data:
+  username: $(echo -n "iamapikey" | openssl base64 -a -A) # Should be aWFtYXBpa2V5
+  password: $(openssl base64 -a -A < knative-builder-key.json)
+EOF
+
+readonly EXIT=$?
+
+rm knative-builder-key.json
+
+exit $EXIT
